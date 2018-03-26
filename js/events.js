c644c6fea2980b89a026dfd26b393cfd73db74bd
Chris Ernst <cernst3@gatech.edu>
2018-03-26 17:40:06 -0400

    human time instead of js time

--- js/events.js
+++ js/events.js
@@ -1,11 +1,13 @@
 // https://gleeclub.gatech.edu/dev/matt/api.php?action=events
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 var events = 
 	{"events":
 		[
 			{
 				"id":"2253",
 				"name":"Spring Semester Concert! Invite your friends!",
-				"time":"1523318400",
+				"time":"1523318490",
 				"location":"WV175",
 				"summary":"Come spend an evening with the Glee Club!",
 				"description":""
@@ -23,8 +25,21 @@ var events =
 	};
 Vue.component('event', {
 	props:['eventprop'],
-  template: '<div class="event"><h3>{{eventprop.name}}</h3><p>{{eventprop.time}}</p><p class="location">{{eventprop.location}}</p><p>{{eventprop.description}}</p><p><a href="#">Put in your calendar and smoke it.</a></p></div>'
+	template: '<div class="event"><h3>{{eventprop.name}}</h3><p>{{humanTime(eventprop.time)}}</p><p class="location">{{eventprop.location}}</p><p>{{eventprop.description}}</p><p><a href="#">Put in your calendar and smoke it.</a></p></div>',
+	methods:{
+		humanTime: function(jsTime){
+			//Friday, March 23, 2018 at 8:00pm
+			jsTime = jsTime*1000.0;
+			var tempDate = new Date();
+			tempDate.setTime(jsTime);
+			var ampm;
+			tempDate.getHours()<12 ? ampm="am" : ampm="pm";
+			return ""+days[tempDate.getDay()]+", "+months[tempDate.getMonth()]+" "+tempDate.getDate()+", "+tempDate.getFullYear()+" at "+(tempDate.getHours()%12)+":"+(tempDate.getMinutes()<10 ? "0"+tempDate.getMinutes() : tempDate.getMinutes())+ampm;
+		}
+	}
 });
+
+Vue.config.devtools = true;
 
 var eventsapp = new Vue({
   el: '#events',
